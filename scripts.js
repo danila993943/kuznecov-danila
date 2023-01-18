8aecf6ab13b6cd927f2d261ff27e3ae26facb4c4
danila993943 <122990561+danila993943@users.noreply.github.com>
2023-01-18 18:55:39 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,85 @@
+//-------------------1
+//переменные:myDog, years , guests
+//Встроенные функции: console.log
+//Функции:,makeTea , secret
+//Аргументы: (guests, 'Earl Grey');(myDog, 4);
+//Параметры:(dogname, age), (cups, tea)
+
+
+//2
+//let name = 'Js';
+//console.log(name.toLowerCase());
+
+
+//3
+//let name = 'Я люблю JS!';
+//console.log(name.substr(2,5));
+//console.log(name.substring(2,10));
+//console.log(name.slice(2,8));
+
+
+//4
+//let name = 'Я люблю JS!';
+//console.log(name.indexOf('люблю'))
+
+
+
+//5
+//let txt = 'Artyr';
+//let n = 5;
+//let result = 0;
+//if (n<txt.length){
+     //result = txt.substr(0,6);
+     //console.log(result + '...');
+//}else {
+    //result=txt;
+    //console.log(result);
+//}    
+
+
+
+
+//6
+//let name= 'Я люблю JS!';
+//console.log(name.replace(/-/g, '!'))
+
+
+
+//7
+//let name = 'я люблю Js'
+//console.log(name.split(''))
+
+
+
+//8
+//let name = 'Привет мир'
+//console.log(name.split(''))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
